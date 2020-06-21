4dc173f6902c5e5a60b526d0f122f584c827db12
Ryan Werner <rmwerner@umich.edu>
2020-06-21 16:56:07 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,17 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""  # add the code snippet here!
+  passengers.each do |key, value|
+    if key == :suite_a && value[0] == "A"
+      winner = value
+    end
+  end
+  winner
 end
